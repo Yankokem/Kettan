f28b44c3cf86d07976a362781d50b09d5fa291b6
Russel Labiaga <153169032+Yankokem@users.noreply.github.com>
2026-05-06 04:50:20 +0800

    login fix

---
+++ Kettan.Server/Data/Migrations/20260505204428_AddReportingFieldsAndUserStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kettan.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReportingFieldsAndUserStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ShippingCost",
+                table: "Shipments",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShippingCost",
+                table: "Shipments");
+        }
+    }
+}
